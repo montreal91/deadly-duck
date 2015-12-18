36568abac55c7cbd9bb040d9dd30684f80077793
Alexander <nefedov.alexander91@yandex.ru>
2015-12-19 01:23:11 +0500

    New JClub class
    Class is well-integrated with old classes. Now gamer has to choose
    a player who is up to play next match.
    
    New file:
    	club.py
    
    Modified:
    	game.py
    	league.py
    	match.py

--- match.py
+++ match.py
@@ -2,13 +2,13 @@
 class JMatch(object):
     def __init__(self, home_team_id=None, away_team_id=None, day=0):
         super(JMatch, self).__init__()
-        self._home_team_id      = home_team_id
-        self._away_team_id      = away_team_id
-        self._day               = day
+        self._home_team_id = home_team_id
+        self._away_team_id = away_team_id
+        self._day = day
 
-        self._home_team_points  = 0
-        self._away_team_points  = 0
-        self._is_played         = False
+        self._home_team_points = 0
+        self._away_team_points = 0
+        self._is_played = False
 
     @property
     def home_team(self):
@@ -33,9 +33,9 @@ class JMatch(object):
 
     @score.setter
     def score(self, result):
-        self._home_team_points  = result[0]
-        self._away_team_points  = result[1]
-        self._is_played         = True
+        self._home_team_points = result[0]
+        self._away_team_points = result[1]
+        self._is_played = True
 
     @property
     def day(self):
@@ -46,9 +46,15 @@ class JMatch(object):
         assert isinstance( val, int )
         self._day = val
 
+    @property
+    def is_played(self):
+        return  self._is_played
+
+    def SetMatchPlayed(self):
+        self._is_played = True
+
     def __str__(self):
         return "<{0:d}> vs <{1:d}>".format(self._home_team_id, self._away_team_id)
 
     def __repr__(self):
         return "JMatch <{0:d}> vs <{1:d}> on {2:d}".format(self._home_team_id, self._away_team_id, self._day)
-    
