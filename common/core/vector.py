360f66a9e8e28bbd32a0b219784aa16527a6613e
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2019-12-09 21:06:33 +0700

    Set alphabetical order of Vector methods
    This simplifies navigation if IDE is unavailable (i.e. online)

--- common/core/vector.py
+++ common/core/vector.py
@@ -17,12 +17,6 @@ class Vector:
 
     _vector: Tuple[float, ...]
 
-    def __init__(self, values: Tuple[float, ...]):
-        self._vector = values
-
-    def __len__(self):
-        return len(self._vector)
-
     def __add__(self, other: "Vector") -> "Vector":
         if len(self) != len(other):
             raise ValueError(
@@ -31,6 +25,21 @@ class Vector:
         return Vector(
             tuple(a + b for a, b in zip(self._vector, other._vector))
         )
+
+    def __eq__(self, other) -> bool:
+        return self._vector == other._vector
+
+    def __init__(self, values: Tuple[float, ...]):
+        self._vector = values
+
+    def __len__(self):
+        return len(self._vector)
+
+    def __mul__(self, scalar: float) -> "Vector":
+        return Vector(tuple(a * scalar for a in self._vector))
+
+    def __ne__(self, other) -> bool:
+        return not self == other
 
     def __sub__(self, other: "Vector") -> "Vector":
         if len(self) != len(other):
@@ -41,44 +50,9 @@ class Vector:
             tuple(a - b for a, b in zip(self._vector, other._vector))
         )
 
-    def __mul__(self, scalar: float) -> "Vector":
-        return Vector(tuple(a * scalar for a in self._vector))
-
-    def __eq__(self, other) -> bool:
-        return self._vector == other._vector
-
-    def __ne__(self, other) -> bool:
-        return not self == other
-
 
 
 class VectorTestCase(TestCase):
-    def test_smoke(self):
-        """
-        Smoke test for vector class.
-
-        Creates four different vectors and checks simple logic of length
-        function and comparing operators.
-        """
-        v1 = Vector((1, 2))
-        v2 = Vector((1, 2, 3))
-        v3 = Vector((1, 2, 4))
-        v4 = Vector((1, 2, 4))
-        self.assertEqual(len(v1), 2)
-        self.assertEqual(len(v2), 3)
-        self.assertEqual(len(v3), 3)
-        self.assertEqual(len(v4), 3)
-
-        self.assertFalse(v1 == v2)
-        self.assertTrue(v1 != v2)
-
-        self.assertFalse(v2 == v3)
-        self.assertTrue(v3 != v2)
-
-        self.assertTrue(v3 == v4)
-        self.assertFalse(v4 != v3)
-
-
     def test_arithmetic(self):
         """
         Smoke test for basic vector arithmetic.
@@ -113,3 +87,28 @@ class VectorTestCase(TestCase):
 
         v2 *= 0
         self.assertEqual(v2, Vector((0, 0)))
+
+    def test_smoke(self):
+        """
+        Smoke test for vector class.
+
+        Creates four different vectors and checks simple logic of length
+        function and comparing operators.
+        """
+        v1 = Vector((1, 2))
+        v2 = Vector((1, 2, 3))
+        v3 = Vector((1, 2, 4))
+        v4 = Vector((1, 2, 4))
+        self.assertEqual(len(v1), 2)
+        self.assertEqual(len(v2), 3)
+        self.assertEqual(len(v3), 3)
+        self.assertEqual(len(v4), 3)
+
+        self.assertFalse(v1 == v2)
+        self.assertTrue(v1 != v2)
+
+        self.assertFalse(v2 == v3)
+        self.assertTrue(v3 != v2)
+
+        self.assertTrue(v3 == v4)
+        self.assertFalse(v4 != v3)
