8b41e0b3531f906d1c76ef20930dc9317257ed4d
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2019-12-09 20:34:56 +0700

    Basic class for vectors

---
+++ run_tests.py
@@ -0,0 +1,16 @@
+
+"""
+Entry point for tests.
+
+Created 2019.12.09
+
+@author montreal91
+"""
+
+from unittest import main
+
+from common.core.vector import VectorTestCase
+
+
+if __name__ == '__main__':
+    main()
