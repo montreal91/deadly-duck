d01f0950a74f651f051bf101b819e326ff0b0d74
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2026-08-25 01:18:58 +0500

    [MVP] Resolutions: Part 11. Finalization

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from kivy.config import Config
 
 from persistence.migration_history import init_db
 
+# Tested resolutions are:
+# - 1366x768
+# - 1920x1080
+# - 2560x1600
+
 # Needs to be here to take an effect
 Config.set("graphics", "fullscreen", "auto")
 Config.set("graphics", "borderless", "1")
 Config.set("graphics", "resizable", "0")
-
 
 if __name__ == '__main__':
     from client import DuckClientApp
